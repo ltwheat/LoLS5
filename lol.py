3eda228dcba72fe6dddb48f8bb4f4e26ae77c5a9
Zach Huffman <zhuffman90@gmail.com>
2015-02-08 13:14:54 -0600

    Added several more functions, mostly concerning match participants and teams. __main__ now prints a brief synopsis of the last match.

--- lol.py
+++ lol.py
@@ -1,7 +1,9 @@
 #C:\Python33\python
 
-import pymongo
+import datetime
+import math
 import json
+import pymongo
 from urllib import request
 
 ltwheat_summ_id = 28767867
@@ -12,7 +14,11 @@ mongo_host = "localhost"
 mongo_port = 27017
 
 ##### TODO #####
-# 1) Make generic api call method, to fail if non-200 status code or json
+# 1) Long-term goal: I need to store all this info as Python objects, rather
+#    than raw data. So a lot of this stuff is gonna have to change, and we'll
+#    also need some actual classes and a api-to-class constructor, but for now,
+#    I'm just hitting the API for everything and dealing with the raw data.
+# 2) Make generic api call method, to fail if non-200 status code or json
 def make_generic_request(url, api_key=None):
     # Default to Lt Wheat
     if api_key == None:
@@ -55,6 +61,46 @@ def get_match_champs(match):
         champ = get_champ_by_id(participant['championId'])
         champs.append(champ)
     return champs
+
+def get_match_participant(match, summ_id=ltwheat_summ_id):
+    participant = None
+    participant_id = get_participant_id(match, summ_id)
+    for p in match['participants']:
+        if p['participantId'] == participant_id:
+            participant = p
+    return participant
+
+def get_match_winner(match):
+    winner = None
+    for team in match['teams']:
+        if team['winner'] == True:
+            winner = team['teamId']
+    return winner
+
+def get_participant_id(match, summ_id=ltwheat_summ_id):
+    participant_idents = match['participantIdentities']
+    participant_id = -1
+    for ident in participant_idents:
+        if ident['player']['summonerId'] == summ_id:
+            participant_id = ident['participantId']
+    return participant_id
+
+def is_winner(match, summ_id=ltwheat_summ_id):
+    participant = get_match_participant(match, summ_id)
+    winning_team_id = get_match_winner(match)
+    won = participant['teamId'] == winning_team_id
+    return won
+
+def match_synopsis(match, summ_id=ltwheat_summ_id):
+    outcome = "Win"
+    if not is_winner(match):
+        outcome = "Loss"
+    participant = get_match_participant(match)
+    champ = get_champ_by_id(participant['championId'])
+    match_creation = math.floor(match['matchCreation']/1000)
+    date_time_played = datetime.datetime.fromtimestamp(match_creation)
+    synopsis = "{0} as {1}, {2}".format(outcome, champ, date_time_played)
+    return synopsis
     
 def get_matches(ranked_queues='',begin_index=-1,end_index=-1,champion_id=-1):
     # TODO:
@@ -86,5 +132,10 @@ def store_last_match():
         print("Could not connect to database")
     
 if __name__ == '__main__':
-    # TODO: This should return a short synopsis of last game, ie "Win as Jinx", maybe date/duration, etc
-    print("name works")
+    # TODO: This should return a short synopsis of last game, ie "Win as Jinx",
+    #       maybe date/duration, etc
+    match = get_last_match()
+
+    synopsis = match_synopsis(match)
+    print("Last match:")
+    print(synopsis)
